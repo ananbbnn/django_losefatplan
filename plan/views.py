0d48008f02639a0c66723ba591906eba6987469c
harold <ananbbnn@gmail.com>
2025-06-06 16:56:00 +0800

    create_plan功能完成，可存至資料庫

--- plan/views.py
+++ plan/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Plan
 # Create your views here.
 
 def create_plan(request):
@@ -7,8 +7,31 @@ def create_plan(request):
     View to create a new plan.
     """
     if request.method == 'POST':
+        name = request.POST.get('name')
+        visibility = request.POST.get('visibility') == 'public'
+        height = request.POST.get('height')
+        weight = request.POST.get('weight')
+        start_date = request.POST.get('startDate')
+        end_date = request.POST.get('endDate')
+        month_goal = request.POST.get('monthGoal')
+        final_goal = request.POST.get('finalGoal')
+        description = request.POST.get('description')
+
+        Plan.objects.create(
+            name=name,
+            private=not visibility,
+            height=float(height),
+            weight=float(weight),
+            start_time=start_date,
+            end_time=end_date,
+            month_target=float(month_goal),
+            final_target=float(final_goal),
+            description=description,
+            user=request.user
+        )
+    
+        return redirect('create_plan')
         # Handle form submission and plan creation logic here
-        pass
     else:
         # Render the plan creation form
         return render(request, 'plan/create_plan.html')
